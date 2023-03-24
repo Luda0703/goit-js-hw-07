3a3efe76d538ce312bdd1ac2bff826adc296adc1
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-03-24 15:35:08 +0200

    task-01

---
+++ js/01-gallery.js
@@ -0,0 +1,56 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+
+ const galleryContainer = document.querySelector('.gallery');
+ const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+ galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+ galleryContainer.addEventListener('click', onContainerClick);
+
+// console.log(createGalleryItemsMarkup(galleryItems))
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return `
+        <li class="gallery__item">
+        <a class="gallery__link" href=${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+        </a>
+        </li>
+        `;
+    })
+    .join(' ');
+}
+
+const instance = basicLightbox.create(
+    `
+  <img width="1280" height="auto" src="">`,
+    {
+      onShow: (instance) => {
+        window.addEventListener('keydown', onEscKeyPress);
+      },
+      onClose: (instance) => {
+        window.removeEventListener('keydown', onEscKeyPress);
+      },
+    }
+  );
+  
+  function onContainerClick(evt) {
+    evt.preventDefault();
+    const datasetSource = evt.target.dataset.source;
+    if (!datasetSource) return;
+    instance.element().querySelector('img').src = datasetSource;
+    instance.show();
+  }
+  
+  function onEscKeyPress(evt) {
+    if (evt.code !== 'Escape') return;
+    instance.close();
+  }
